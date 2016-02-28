c6cd7b0499dcbc775643ca1b60cfa01cd8328379
Tylor Stewart <trjstewart@gmail.com>
2016-02-28 21:08:18 +1000

    Bug Fix
    Fixed a bug with modals where you had to click twice for it to open.

--- public/javascripts/codenetwork.js
+++ public/javascripts/codenetwork.js
@@ -8,43 +8,28 @@ app.config(function($routeProvider){
       controller: 'mainController'
     })
     .when('/join', {
-    templateUrl: 'join.html',
-    controller: 'authController'
-  });
+      templateUrl: 'join.html',
+      controller: 'authController'
+    });
 });
 
 app.controller('navController', function($scope, $http){
-  console.log('Using Nav Controller...');
-
   // Nav Controller...
+
 });
 
 app.controller('mainController', function($scope, $http){
-  console.log('Using Main Controller...');
+  // Main Controller...
 
   // Toggle Modals
-  $scope.modal = { join: false };
+  $scope.modal = { join: false, other: false }
   $scope.toggleModal = function(modal) {
     (modal == 'join') ? $scope.modal.join = !$scope.modal.join : null;
-    console.log($scope.modal.join);
-  };
-
-  // Modal : Join Code Network
-  $scope.showModalJoin = false;
-  $scope.toggleModal_Register = function() {
-    $scope.modalJoin = !$scope.modalJoin;
-  };
-
-  // Modal : Example Modal
-  $scope.showModalOther = false;
-  $scope.toggleModalOther = function() {
-    $scope.modalOther = !$scope.modalOther;
-  };
+    (modal == 'other') ? $scope.modal.other = !$scope.modal.other : null;
+  }
 });
 
 app.controller('authController', function($scope, $http){
-  console.log('Using Auth Controller...');
-
   // Auth Controller...
 
 });
@@ -56,19 +41,13 @@ app.directive('modal', function () {
     link: function postLink(scope, element, attrs) {
       scope.title = attrs.title;
 
-      scope.$watch(attrs.visible, function(value){
-        if(value == true) $(element).modal('show');
-        else $(element).modal('hide');
+      scope.$watch(attrs.visible, function(value) {
+        if(value) { return $(element).modal('show'); }
+        else { $(element).modal('hide'); }
       });
 
-      $(element).on('shown.bs.modal', function(){
-        scope.$apply(function(){ scope.$parent[attrs.visible] = true; });
-        console.log(scope.$parent[attrs.visible]);
-      });
-
-      $(element).on('hidden.bs.modal', function(){
-        scope.$apply(function(){ scope.$parent[attrs.visible] = false; });
-        console.log(scope.$parent[attrs.visible]);
+      $(element).on('hidden.bs.modal', function() {
+        scope.$apply(function() { scope.toggleModal(attrs.visible.split('.')[1]); });
       });
     }
   };
