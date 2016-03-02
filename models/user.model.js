6e266fc614e196fff55c2ee0bbd2f28f4fb0386a
Tylor Stewart <trjstewart@gmail.com>
2016-03-02 12:00:07 +1000

    Created User Model

---
+++ models/user.model.js
@@ -0,0 +1,16 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+var userSchema = new Schema({
+  name: { first: String, last: String },
+  email: String,
+  password: String,
+  student: {
+    status: {type: Boolean, default: false},
+    institution: String,
+    number: String,
+    faculty: String
+  }
+});
+
+mongoose.model('User', userSchema);
